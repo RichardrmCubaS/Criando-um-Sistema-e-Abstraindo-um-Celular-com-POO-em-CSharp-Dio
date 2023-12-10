feab34d0c8b798dac4fb3c0d77fe9e06540cdf8f
Richard Cuba <148399329+RichardrmCubaS@users.noreply.github.com>
2023-12-10 18:09:20 -0300

    Add files via upload

---
+++ Models/Iphone.cs
@@ -0,0 +1,15 @@
+namespace DesafioPOO.Models
+{
+    public class Iphone : Smartphone
+    {
+
+        public Iphone(string numero, string modelo, string iMEI, int memoria) : base(numero, modelo, iMEI, memoria)
+        {
+        }
+        
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativo {nomeApp} no Iphone...");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Teste do Iphone");
+Smartphone iphone = new Iphone(numero: "99111-1111", modelo: "14", imei: "111111111", memoria: 128);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+   
+
+Console.WriteLine("---------------------------------");
+
+
+Console.WriteLine("Teste do nokia");
+Smartphone nokia = new Nokia(numero: "98222-2222", modelo: "N95", imei: "000000000", memoria: 64);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("TikTok");
+    
